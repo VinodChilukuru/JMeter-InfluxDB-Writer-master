f91ff69e32c59114c642d18d6d1688f9d901ae25
Vinod Chilukuru <vchilukuru@asiapac.globalcsc.net>
2017-04-14 13:55:01 +0530

    code commit

---
+++ src/main/java/rocks/nt/apm/jmeter/config/influxdb/RequestMeasurement.java
@@ -0,0 +1,60 @@
+package rocks.nt.apm.jmeter.config.influxdb;
+
+/**
+ * Constants (Tag, Field, Measurement) names for the requests measurement.
+ * 
+ * @author Alexander Wert
+ *
+ */
+public interface RequestMeasurement {
+
+	/**
+	 * Measurement name.
+	 */
+	String MEASUREMENT_NAME = "requestsRaw";
+	
+	
+	/**
+	 * Fields.
+	 * 
+	 * @author Alexander Wert
+	 *
+	 */
+	public interface Fields {	
+		/**
+		 * Test name field.
+		 */
+		String TEST_NAME = "testName";
+	}
+
+	/**
+	 * Tags.
+	 * 
+	 * @author Alexander Wert
+	 *
+	 */
+	public interface Tags {
+		/**
+		 * Request name tag.
+		 */
+		String REQUEST_NAME = "requestName";
+	}
+
+	/**
+	 * Fields.
+	 * 
+	 * @author Alexander Wert
+	 *
+	 */
+	public interface Fields {
+		/**
+		 * Response time field.
+		 */
+		String RESPONSE_TIME = "responseTime";
+
+		/**
+		 * Error count field.
+		 */
+		String ERROR_COUNT = "errorCount";
+	}
+}
